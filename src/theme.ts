fd4296bd84d53728c77c0a53003ea31325c41f1a
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-06 21:37:52 +0900

    4.1 Styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "#111",
-  btnColor: "tomato",
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#fbc531",
 };
